cb5b580c4d73fcd089e839d0d59d418e8afc79d8
bladetin <bladetins@gmail.com>
2021-12-23 11:08:01 +0800

    GPU_first

--- autoencoder.py
+++ autoencoder.py
@@ -5,14 +5,15 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 
-# device = torch.device("cuda:0")
+device_gpu = torch.device("cuda:1")
+# print(torch.cuda.is_available())
 
 transform = transforms.ToTensor()
 mnist_data = datasets.MNIST(
     root=".\data", train=True, download=True, transform=transform
 )
 data_loader = torch.utils.data.DataLoader(
-    dataset=mnist_data, batch_size=5000, shuffle=True
+    dataset=mnist_data, batch_size=128, shuffle=True, num_workers=1,pin_memory=True
 )
 
 # train_data -> data
@@ -58,46 +59,50 @@ class Autoencoder(nn.Module):
 
 
 model = Autoencoder()
-# model.to(device)
-criterion = nn.MSELoss()
+model = model.to(device_gpu)
+criterion = nn.MSELoss().to(device_gpu)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
 
-num_epochs = 20
+num_epochs = 5
 outputs = []
-for epoch in range(num_epochs):
-    # for img in data_loader.dataset.data.cpu():
-    for (img, _) in data_loader:
-        # img = img.reshape(-1, 28 * 28).to(device)
-        img = img.reshape(-1, 28 * 28)
-        recon = model(img)
-        loss = criterion(recon, img)
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
 
-    print(f"Epoch {epoch+1}, Loss:{loss.item():.4f}")
-    outputs.append((epoch, img, recon))
+if __name__ == '__main__':
+    for epoch in range(num_epochs):
+        # for img in data_loader.dataset.data.cpu():
 
-for k in range(0, num_epochs, 4):
-    plt.figure(figsize=(9, 2))
-    plt.gray()
-    imgs = outputs[k][1].detach().numpy()
-    recon = outputs[k][2].detach().numpy()
-    for i, item in enumerate(imgs):
-        if i >= 9:
-            break
-        plt.subplot(2, 9, i + 1)
-        item = item.reshape(-1, 28, 28)
-        # plt.imshow(item[0])
+        for i, (img, labels) in enumerate(data_loader):
+            # img = img.reshape(-1, 28 * 28).to(device)
+            img = torch.reshape(img, (-1, 28 * 28)).to(device_gpu)
+            recon = model(img)
+            loss = criterion(recon, img)
 
-    for i, item in enumerate(recon):
-        if i >= 9:
-            break
-        plt.subplot(2, 9, 9 + i + 1)
-        item = item.reshape(-1, 28, 28)
-        # plt.imshow(item[0])
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-myfig = plt.gcf()
-myfig.savefig(".\figure\figure.png", dpi=300)
+        print(f"Epoch {epoch+1}, Loss:{loss.item():.4f}")
+        outputs.append((epoch, img, recon))
+
+    for k in range(0, num_epochs, 4):
+        plt.figure(figsize=(9, 2))
+        plt.gray()
+        imgs = outputs[k][1].detach().numpy()
+        recon = outputs[k][2].detach().numpy()
+        for i, item in enumerate(imgs):
+            if i >= 9:
+                break
+            plt.subplot(2, 9, i + 1)
+            item = item.reshape(-1, 28, 28)
+            # plt.imshow(item[0])
+
+        for i, item in enumerate(recon):
+            if i >= 9:
+                break
+            plt.subplot(2, 9, 9 + i + 1)
+            item = item.reshape(-1, 28, 28)
+            # plt.imshow(item[0])
+
+    myfig = plt.gcf()
+    myfig.savefig(".\figure\figure.png", dpi=300)
 
